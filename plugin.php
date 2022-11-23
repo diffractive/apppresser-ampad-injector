88318793d55841795ba245c2087eca957495ffd0
Franky Hung <franky@diffractive.io>
2022-11-23 17:11:27 +0800

    updated license info, fixed minor issues, added important notice below textarea

--- plugin.php
+++ plugin.php
@@ -6,22 +6,8 @@ Description: This plugin lets you insert arbitrary HTML in posts served in the A
 Version:     1.0.0
 Author:      Diffractive.io
 Author URI:  http://diffractive.io
-License:     GPL3
-License URI: https://www.gnu.org/licenses/gpl-3.0.html
-
-Copyright 2022 Diffractive.io (email: info@diffractive.io)
-(AppPresser HTML Inserter) is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
- 
-(AppPresser HTML Inserter) is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License
-along with (AppPresser HTML Inserter). If not, see (https://diffractive.io to your plugin license).
+License:     MIT License
+License URI: https://github.com/diffractive/apppresser-html-inserter/blob/main/LICENSE.md
 */
 
 function str_replace_n_after($search, $replace, $subject, $occurrence) {
@@ -34,7 +20,7 @@ function insert_html_in_app_posts ( $content ) {
 	// only insert html if it is a wp json request, and it's not serving pages
 	// currently amp-ad tags are either wiped or not served in the app content for unknown reasons
 	if (wp_is_json_request() && strpos( $_SERVER['REQUEST_URI'], 'wp/v2/pages') === FALSE) {
-		return str_replace_n_after("</p>", get_option('html-content', ""), $content, 3);
+		return str_replace_n_after("</p>", get_option('html-content', ""), $content, (int)get_option('insert-after-paragraph-num', 3));
 	} else {
 		return $content;
 	}
@@ -65,17 +51,21 @@ function register_apppresser_html_inserter_settings() {
 function apppresser_html_inserter_options_page() {
 ?>
 <div class="wrap">
-	<h2>AppPresser Ad inserter</h2>
+	<h2>AppPresser HTML inserter</h2>
 	<form method="post" action="options.php">
 		<?php settings_fields( 'appp-html-inserter-group' ); ?>
 		<?php do_settings_sections( 'appp-html-inserter-group' ); ?>
 		<table class="form-table">
 			<tr valign="top">
 				<th scope="row">Raw HTML Content</th>
-				<td><textarea name="html-content" rows="8" style="width: 100%"><?php echo get_option('html-content'); ?></textarea></td>
+				<td>
+					<textarea name="html-content" rows="8" style="width: 100%"><?php echo get_option('html-content'); ?></textarea>
+					<br>
+					<p><strong>Important notice:</strong> <i>Note that script tags you insert in the HTML will most likely not get executed in the app. If you want to execute any client-side javascript code, follow the <a href="https://docs.apppresser.com/article/392-custom-javascript" target="_blank">official instructions</a>.</i></p>
+				</td>
 			</tr>
 			<tr valign="top">
-				<th scope="row">Insert after paragraph number<br>(Default: 3)</th>
+				<th scope="row">Insert HTML after paragraph number<br>(Default: 3)</th>
 				<td><input type="number" name="insert-after-paragraph-num" value="<?php echo get_option('insert-after-paragraph-num', 3); ?>" /></td>
 			</tr>
 		</table>
