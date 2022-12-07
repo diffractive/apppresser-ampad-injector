d4ba573292aeaeda7224ba94004be01fca1ec5c0
guilhbr <guilherme.quadros.souza@gmail.com>
2022-12-07 14:23:18 +0800

    DIFF-40 - Add prefixes to functions

--- plugin.php
+++ plugin.php
@@ -9,24 +9,24 @@ License:     MIT License
 License URI: https://github.com/diffractive/apppresser-ampad-injector/blob/main/LICENSE.md
 */
 
-function str_replace_n_after($search, $replace, $subject, $occurrence) {
+function apaai_str_replace_n_after($search, $replace, $subject, $occurrence) {
 	$search = preg_quote($search,'/');
 	return preg_replace("/^((?:.*?$search){".$occurrence."})/s", "$1$replace", $subject);
 }
 
-function amp_ad_content($data_option) {
+function apaai_amp_ad_content($data_option) {
 	return "<amp-ad width=336 height=280 type=\"doubleclick\" data-slot=\"$data_option\" data-multi-size=\"300x250\"></amp-ad>";
 }
 
 /* Inject AMP Ad into Posts. */
-function insert_amp_ad_in_app_posts ( $content ) {
+function apaai_insert_amp_ad_in_app_posts ( $content ) {
 	// only insert html if it is a wp json request for posts, note that we tried wp_is_json_request() but didn't work in the app (worked in preview)
 	// currently amp-ad tags are either wiped or not served in the app content for unknown reasons
 	if (strpos( $_SERVER['REQUEST_URI'], '/wp-json/wp/v2/posts') === 0) {
 		// insert first block
-		$content = str_replace_n_after("</p>", amp_ad_content(get_option('data-slot-1', "")), $content, (int)get_option('insert-after-paragraph-num-1', 3));
+		$content = apaai_str_replace_n_after("</p>", apaai_amp_ad_content(get_option('data-slot-1', "")), $content, (int)get_option('insert-after-paragraph-num-1', 3));
 		// insert second block
-		$content = str_replace_n_after("</p>", amp_ad_content(get_option('data-slot-2', "")), $content, (int)get_option('insert-after-paragraph-num-2', 8));
+		$content = apaai_str_replace_n_after("</p>", apaai_amp_ad_content(get_option('data-slot-2', "")), $content, (int)get_option('insert-after-paragraph-num-2', 8));
 		return $content;
 	} else {
 		return $content;
@@ -35,30 +35,30 @@ function insert_amp_ad_in_app_posts ( $content ) {
 
 // add this filter to the 'the_content' hook, see https://developer.wordpress.org/reference/hooks/the_content/
 // pick a larger number to ensure a later priority, to reduce possibility of tags being sanitized away by other filters
-add_filter( 'the_content', 'insert_amp_ad_in_app_posts', 90000);
+add_filter( 'the_content', 'apaai_insert_amp_ad_in_app_posts', 90000);
 
 /**
  * Registers a new options page: 'AppPresser Ad inserter settings page under Settings
  */
-function register_apppresser_amp_ad_inserter_option_page() {
+function apaai_register_apppresser_amp_ad_inserter_option_page() {
     add_options_page(
         'AppPresser AMP Ad Injector',
         'AppPresser AMP Ad Injector',
         'publish_pages',
         'apppresser-amp-ad-inserter',
-        'apppresser_amp_ad_inserter_options_page',
+        'apaai_apppresser_amp_ad_inserter_options_page',
         999
     );
-	add_action( 'admin_init', 'register_apppresser_amp_ad_inserter_settings' );
+	add_action( 'admin_init', 'apaai_register_apppresser_amp_ad_inserter_settings' );
 }
-function register_apppresser_amp_ad_inserter_settings() {
+function apaai_register_apppresser_amp_ad_inserter_settings() {
 	register_setting('appp-amp-ad-inserter-group', 'data-slot-1');
 	register_setting('appp-amp-ad-inserter-group', 'insert-after-paragraph-num-1');
 
 	register_setting('appp-amp-ad-inserter-group', 'data-slot-2');
 	register_setting('appp-amp-ad-inserter-group', 'insert-after-paragraph-num-2');
 }
-function apppresser_amp_ad_inserter_options_page() {
+function apaai_apppresser_amp_ad_inserter_options_page() {
 ?>
 <div class="wrap">
 	<h2>AppPresser AMP Ad Injector</h2>
@@ -102,6 +102,6 @@ function apppresser_amp_ad_inserter_options_page() {
 </div>
 <?php
 }
-add_action( 'admin_menu', 'register_apppresser_amp_ad_inserter_option_page' );
+add_action( 'admin_menu', 'apaai_register_apppresser_amp_ad_inserter_option_page' );
 
 ?>
